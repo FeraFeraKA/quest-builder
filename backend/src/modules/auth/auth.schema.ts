9cbde4b7cf60c20daa9ba5b0572f1bcf812b2e5c
Fera <feraferaca@gmail.com>
2026-03-24 19:10:41 +0300

    fix: add trim to validation schema

--- backend/src/modules/auth/auth.schema.ts
+++ backend/src/modules/auth/auth.schema.ts
@@ -4,8 +4,9 @@ const RegisterLoginSchema = z.object({
   nickname: z
     .string()
     .min(1, "Nickname is required")
-    .max(100, "Nickname must be shorter that 100 characters"),
-  password: z.string().min(8, "Password too short"),
+    .max(100, "Nickname must be shorter that 100 characters")
+    .trim(),
+  password: z.string().min(8, "Password too short").trim(),
 });
 
 export default RegisterLoginSchema;
