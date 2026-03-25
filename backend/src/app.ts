9c676c066ce895f1c085c5979e06983188957937
Fera <feraferaca@gmail.com>
2026-03-25 20:10:34 +0300

    feat: add cors and helmet to app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,21 @@
+import cors from "cors";
+import "dotenv/config";
 import type { Express } from "express";
 import express from "express";
+import helmet from "helmet";
 import authRouter from "./modules/auth/auth.routes";
 
 const app: Express = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+
+app.use(helmet());
 
 app.use(express.json());
 
--- backend/src/types/env.d.ts
+++ backend/src/types/env.d.ts
@@ -1,6 +1,7 @@
 declare namespace NodeJS {
   interface ProcessEnv {
     PORT: number;
+    CORS_ORIGIN: string;
     DATABASE_URL: string;
   }
 }
