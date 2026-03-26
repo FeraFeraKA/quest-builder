e420786f4d951d468fc36c3e9e3ec23b6b376950
Fera <feraferaca@gmail.com>
2026-03-26 20:36:12 +0300

    feat: start using error and route middlewares in app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,6 +4,8 @@ import express from "express";
 import helmet from "helmet";
 import authRouter from "./modules/auth/auth.routes";
 import { config } from "./shared/config/env";
+import { errorHandler } from "./shared/middleware/errorHandler";
+import { routeHandler } from "./shared/middleware/routeHandler";
 
 const app: Express = express();
 
@@ -19,6 +21,10 @@ app.use(helmet());
 
 app.use(express.json());
 
+app.use(errorHandler);
+
 app.use("/auth", authRouter);
+
+app.use(routeHandler);
 
 export default app;
