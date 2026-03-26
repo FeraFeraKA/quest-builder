c58e9ad06e44067d9a55e85de2ac5f1a6c0f0f35
Fera <feraferaca@gmail.com>
2026-03-26 11:46:05 +0300

    refactor: centralize env variables in env.ts

---
+++ backend/src/shared/config/env.schema.ts
@@ -0,0 +1,14 @@
+import "dotenv/config";
+import z from "zod";
+
+const EnvSchema = z.object({
+  PORT: z.coerce.number(),
+  CORS_ORIGIN: z.string(),
+  DATABASE_URL: z.string(),
+  JWT_ACCESS_SECRET: z.string(),
+  JWT_REFRESH_SECRET: z.string(),
+  JWT_ACCESS_EXPIRES_IN: z.string(),
+  JWT_REFRESH_EXPIRES_IN: z.string(),
+});
+
+export const env = EnvSchema.parse(process.env);
---
+++ backend/src/shared/config/env.ts
@@ -0,0 +1,12 @@
+import type { StringValue } from "ms";
+import { env } from "./env.schema";
+
+export const config = {
+  port: env.PORT,
+  corsOrigin: env.CORS_ORIGIN,
+  databaseUrl: env.DATABASE_URL,
+  jwtAccessSecret: env.JWT_ACCESS_SECRET,
+  jwtRefreshSecret: env.JWT_REFRESH_SECRET,
+  jwtAccessExpiresIn: env.JWT_ACCESS_EXPIRES_IN as StringValue,
+  jwtRefreshExpiresIn: env.JWT_REFRESH_EXPIRES_IN as StringValue,
+};
