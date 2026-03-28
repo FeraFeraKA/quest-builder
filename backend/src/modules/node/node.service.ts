2683fb23b4a5e4db3c31347177e0e7f2e847b312
Fera <feraferaca@gmail.com>
2026-03-28 20:26:43 +0300

    fix: add await when getting quest

--- backend/src/modules/node/node.service.ts
+++ backend/src/modules/node/node.service.ts
@@ -4,17 +4,17 @@ import { NodeStorage } from "./node.storage";
 import type { INodePayload } from "./node.types";
 
 export const NodeService = {
-  async create(payload: INodePayload) {
-    const quest = QuestStorage.getQuest({
-      questId: payload.data.questId,
-      userId: payload.userId,
+  async create({ data, userId }: INodePayload) {
+    const quest = await QuestStorage.getQuest({
+      questId: data.questId,
+      userId,
     });
 
     if (!quest) {
       throw new HttpError(404, "NOT_FOUND", "Quest not found");
     }
 
-    const node = await NodeStorage.create(payload.data);
+    const node = await NodeStorage.create(data);
 
     if (!node) {
       throw new HttpError(400, "INVALID_DATA", "Invalid data");
