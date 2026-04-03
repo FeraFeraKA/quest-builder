ccc1177e30d907476539f5ff27d4b6a00cbb4648
Fera <feraferaca@gmail.com>
2026-04-03 19:35:43 +0300

    chore: add tanstack eslint plugin

--- frontend/src/components/ui/Logo.tsx
+++ frontend/src/components/ui/Logo.tsx
@@ -6,7 +6,7 @@ const Logo = () => {
       <div className="w-50 h-auto">
         <Link to="/">
           <img
-            src="/images/Logo.png"
+            src="/images/logo.png"
             alt="Quest Builder"
             className="w-fit h-fit"
           />
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,12 +4,24 @@ import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 import Layout from "./components/layout/MainLayout";
 import "./index.css";
+import Login from "./pages/Login";
 import MainPage from "./pages/MainPage";
+import Register from "./pages/Register";
 
 const router = createBrowserRouter([
   {
     Component: Layout,
-    children: [{ path: "/", Component: MainPage }],
+    children: [
+      { path: "/", Component: MainPage },
+      {
+        path: "/auth/register",
+        Component: Register,
+      },
+      {
+        path: "/auth/login",
+        Component: Login,
+      },
+    ],
   },
 ]);
 
